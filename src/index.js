6f9232ffbd907915c933f505378c0e5a88801246
KolyaAlavackii <nikolayalavackii@gmail.com>
2016-12-14 19:25:36 +0200

    Готово

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 ﻿
+var areIntersected = function(rect1, rect2) {
+     if (rect1.top + rect1.width > rect2.top &&
+         rect1.left + rect1.height > rect2.left &&
+         rect1.left < rect2.left + rect2.height) {
+         return true;
+    }
+      else {
+        return false;
+    }
+}
+
+
+
+var filterVisible = function(rect, array) {
+     var arrayRect = array.filter(function(element){
+       if(areIntersected(rect, element) && (element.width && element.height > 0)){
+           return element;
+         }
+       })
+     return arrayRect;
+   }
+
